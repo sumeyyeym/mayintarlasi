47cf611918521cd141991b143b0340f89f1fe749
sumeyyeym <56842981+sumeyyeym@users.noreply.github.com>
2019-11-11 02:31:00 +0300

    Add project files.

---
+++ mayin/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace mayin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpBolge = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flpBolge
+            // 
+            this.flpBolge.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flpBolge.Location = new System.Drawing.Point(12, 12);
+            this.flpBolge.Name = "flpBolge";
+            this.flpBolge.Size = new System.Drawing.Size(515, 515);
+            this.flpBolge.TabIndex = 0;
+            this.flpBolge.Paint += new System.Windows.Forms.PaintEventHandler(this.flpBolge_Paint);
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.Location = new System.Drawing.Point(12, 533);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(515, 46);
+            this.btnBasla.TabIndex = 1;
+            this.btnBasla.Text = "BAŞLA";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 590);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.flpBolge);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpBolge;
+        private System.Windows.Forms.Button btnBasla;
+    }
+}
+
---
+++ mayin/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+
+        private void flpBolge_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void Mayinalani_Click(object sender, EventArgs e)
+        {
+
+            Bolge bolge = (Bolge)sender;
+
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            flpBolge.Controls.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                Button mayinalani = new Button();
+                mayinalani.Width = mayinalani.Height = 45;
+                mayinalani.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+
+                int mayin = rnd.Next(0, 2);
+                mayinalani.Text = mayin.ToString();
+                mayinalani.Tag = mayin;
+                mayinalani.Click += Mayinalani_Click;
+
+                flpBolge.Controls.Add(mayinalani);
+            }
+        }
+
+        class Bolge: Button
+        {
+            private bool mayinlimi;
+            public bool MayinliMi { get { return mayinlimi; } }
+
+            private bool acildimi;
+            public bool AcildiMi { get { return acildimi; } }
+
+            private bool koordinatX;
+            public bool KoordinatX { get { return koordinatX; } }
+
+            private bool koordinatY;
+            public bool KoordinatY { get { return koordinatX; } }
+
+            void BolgeAc(int mayinsayisi)
+            {
+                if (mayinsayisi != 0)
+                {
+                    this.Text = mayinsayisi.ToString();
+                    this.BackColor = Color.White;
+                }
+                this.Enabled = false;
+                this.acildimi = true;
+
+            }
+            void BolgeAc()
+            {
+                if (mayinlimi == false)
+                {
+                    this.BackColor = Color.White;
+                }
+                else
+                {
+                    this.Image = Image.FromFile("C:/Kullanıcılar/User/Downloads/bomb.png");
+                }
+                this.acildimi = true;
+            }
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }  
+}
